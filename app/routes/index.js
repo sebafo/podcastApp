e881c036330f9392256c9aa0878d7b8a88c8ded6
Sebastian Foerg <sebastian.foerg@microsoft.com>
2024-06-10 12:27:57 +0200

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res) {
-  try {
-    const response = await fetch('http://localhost:3000/items');
-    const items = await response.json();
-
-    res.render('index', { items });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ message: 'An error occurred' });
-  }
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
